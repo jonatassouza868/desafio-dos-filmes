93e0701537cee9c3d961f5baae96b4116b825358
Jonatas Souza De Oliveira <jonatasoliveira@brq.com>
2022-11-06 08:54:42 -0300

    desafio dos filmes adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { FilmesComponent } from './filmes/filmes.component';
+import { GeneroComponent } from './genero/genero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UsuarioComponent,
+    FilmesComponent,
+    GeneroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VirtualTimeScheduler } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  texto:string = ''
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+    console.log(this.router.url);
+    if(this.router.url==='/home'){
+      this.texto = 'Conheça nossa Coletânea'
+    }
+    else if(this.router.url==='/usuario'){
+      this.texto = 'Faça o Cadastro de usuários e  edite, caso necessário pronto para cadastrar?'
+    }
+    else if(this.router.url==='/filmes'){
+      this.texto = 'Cadastre os filmes de sua preferência '
+    }
+    else{
+      this.texto = 'Cadastre os gêneros dos filmes'
+    }
+  }
+
+}
